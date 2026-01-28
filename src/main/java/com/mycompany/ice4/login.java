413a8f89e2965be29ea627c37b11e99047bbc675
Faraz Syed <farazali7890@gmail.com>
2026-01-28 15:16:16 -0500

    new login file

---
+++ src/main/java/com/mycompany/ice4/login.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ice4;
+
+/**
+ *
+ * @author faraz
+ */
+public class login {
+    public static void main(String[] args) {
+        System.out.println("Login feature");
+    }
+}
